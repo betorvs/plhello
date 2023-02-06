87c175600564585032423c843126c822df5c3b6d
Roberto Scudeller <beto.rvs@gmail.com>
2023-02-06 15:40:54 +0100

    fix build and push script

--- scripts/build-n-push.sh
+++ scripts/build-n-push.sh
@@ -3,9 +3,18 @@
 REPOSITORY="${1:-betorvs/plhello}"
 TAG="${2:-latest}"
 VERSION=${3:-develop}
+TARGET=${4:-dockerhub}
 
 docker build -t ${REPOSITORY}:${TAG} \
     -f Dockerfile \
     --build-arg BUILD_REF=${VERSION} \
     .
-docker push ${REPOSITORY}:${TAG}
+
+if [ "${TARGET}" == "local" ]; then 
+    # to avoid error when pushing images to local registry in gh workflow
+    docker tag ${REPOSITORY}:${TAG} localhost:5050/plhello:${TAG}
+    docker push localhost:5050/plhello:${TAG}
+else 
+   docker push ${REPOSITORY}:${TAG}
+fi
+
